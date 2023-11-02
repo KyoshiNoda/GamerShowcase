f1e0dc750ff8961e40ffb36edd2f0431e2679238
KyoshiNoda <kyoshisew@gmail.com>
2023-11-01 22:26:35 -0400

    added simple register auth via firebase

--- frontend/src/main/java/com/example/frontend/controllers/RegisterPageController.java
+++ frontend/src/main/java/com/example/frontend/controllers/RegisterPageController.java
@@ -1,31 +1,24 @@
 package com.example.frontend.controllers;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class RegisterPageController {
     @FXML private TextField firstNameInput;
     @FXML private TextField lastNameInput;
     @FXML private TextField emailInput;
-    @FXML private TextField passwordInput;
-    @FXML private TextField confirmPasswordInput;
+    @FXML private PasswordField passwordInput;
+    @FXML private PasswordField confirmPasswordInput;
     @FXML private Button submitButton;
 
-
-    public void initialize() {
-        passwordInput.textProperty().addListener((observable, oldValue, newValue) -> {
-            String maskedPassword = newValue.replaceAll(".", "*");
-            passwordInput.setText(maskedPassword);
-        });
-        confirmPasswordInput.textProperty().addListener((observable, oldValue, newValue) -> {
-            String maskedPassword = newValue.replaceAll(".", "*");
-            confirmPasswordInput.setText(maskedPassword);
-        });
-    }
-
-
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @FXML
     private void submitHandler(ActionEvent event) {
@@ -35,13 +28,19 @@ public class RegisterPageController {
         String password = passwordInput.getText();
         String confirmPassword = confirmPasswordInput.getText();
 
-        // Print the values
-        System.out.println("First Name: " + firstName);
-        System.out.println("Last Name: " + lastName);
-        System.out.println("Email: " + email);
-        System.out.println("Password: " + password);
-        System.out.println("Confirm Password: " + confirmPassword);
+        if (!password.equals(confirmPassword)) {
+            System.out.println("Password and Confirm Password do not match");
+            return;
+        }
+
+        try {
+            UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                    .setEmail(email)
+                    .setPassword(password);
+            UserRecord userRecord = auth.createUser(request);
+            System.out.println("Successfully created user with UID: " + userRecord.getUid());
+        } catch (FirebaseAuthException e) {
+            System.out.println("Error creating user: " + e.getMessage());
+        }
     }
-
-
 }
