ed0565091f0f70b9a70310fc249a49d08acd3620
KyoshiNoda <kyoshisew@gmail.com>
2023-11-11 21:56:56 -0500

    installed BCrypt and changed User property

--- frontend/src/main/java/com/example/frontend/controllers/LoginPageController.java
+++ frontend/src/main/java/com/example/frontend/controllers/LoginPageController.java
@@ -1,6 +1,8 @@
 package com.example.frontend.controllers;
+import com.example.frontend.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -10,9 +12,23 @@ public class LoginPageController {
 
     @FXML
     private void loginButtonHandler(ActionEvent actionEvent) {
-        String email = emailField.getText();
-        String password = passwordField.getText();
-        System.out.println("email: " + email);
-        System.out.println("Password: " + password);
+//        String email = emailField.getText();
+//        String password = passwordField.getText();
+//
+//        try {
+//            App.auth.signInWithEmailAndPassword(email, password);
+//            System.out.println("Login successful!");
+//        } catch (Exception e) {
+//            showAlert("Authentication failed: " + e.getMessage());
+//        }
+        System.out.println("hello");
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Authentication Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
