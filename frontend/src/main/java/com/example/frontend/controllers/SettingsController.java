9b2402142b8df6c9b077fa13173fe11beff78303
kyoshinoda <kyoshisew@gmail.com>
2023-11-22 15:17:25 -0500

    fixed location error

--- frontend/src/main/java/com/example/frontend/controllers/SettingsController.java
+++ frontend/src/main/java/com/example/frontend/controllers/SettingsController.java
@@ -1,45 +1,52 @@
 package com.example.frontend.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import com.example.frontend.App;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField; // Change import to TextField
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
 public class SettingsController {
-    @FXML
-    private Button Change_First_Name_Button;
 
     @FXML
-    private Button Change_Last_Name_Button;
-
-    @FXML
-    private Button Change_Username_Button;
-
-    @FXML
-    private Button Change_Email_Button;
+    private TextField Name_Box; // Change type to TextField
 
     @FXML
     protected void Return_To_Main_Page() {
-
-    }
-    @FXML
-    public void Go_To_Change_First_Name() throws IOException {
-        App.setRoot("ChangeFirstName");
-    }
-    @FXML
-    public void Go_To_Change_Last_Name() throws IOException {
-        App.setRoot("ChangeLastName");
+        // Implement your logic here if needed
     }
 
-    @FXML
-    public void Go_To_Change_Username() throws IOException {
-       App.setRoot("ChangeUsername");
+    private void loadScene(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Scene scene = new Scene(loader.load(), 600, 400);
+
+            Stage stage = (Stage) Name_Box.getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
-    public void Go_To_Change_Email() throws IOException {
-        App.setRoot("ChangeEmail");
+    public void Go_To_Change_First_Name() {
+        loadScene("/com/example/frontend/ChangeFirstName.fxml");
+    }
+
+    @FXML
+    public void Go_To_Change_Last_Name() {
+        loadScene("/com/example/frontend/ChangeLastName.fxml");
+    }
+
+    @FXML
+    public void Go_To_Change_Username() {
+        loadScene("/com/example/frontend/ChangeUsername.fxml");
+    }
+
+    @FXML
+    public void Go_To_Change_Email() {
+        loadScene("/com/example/frontend/ChangeEmail.fxml");
     }
 }
