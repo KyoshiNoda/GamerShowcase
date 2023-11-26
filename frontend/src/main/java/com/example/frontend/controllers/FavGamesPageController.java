d02a572267367ff1a79ca53406258dfc740c2a36
kyoshinoda <kyoshisew@gmail.com>
2023-11-25 22:26:05 -0500

    improved listView UI

--- frontend/src/main/java/com/example/frontend/controllers/FavGamesPageController.java
+++ frontend/src/main/java/com/example/frontend/controllers/FavGamesPageController.java
@@ -5,6 +5,10 @@ import com.example.frontend.User;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.util.Callback;
 
 public class FavGamesPageController {
@@ -21,17 +25,30 @@ public class FavGamesPageController {
                     @Override
                     protected void updateItem(Game game, boolean empty) {
                         super.updateItem(game, empty);
-
                         if (empty || game == null) {
-                            setText(null);
                             setGraphic(null);
+                            setText(null);
                         } else {
-                            setText(game.getName());
+                            VBox vbox = new VBox();
+                            vbox.setStyle("-fx-background-size: contain; " +
+                                    "-fx-background-position: center center; " +
+                                    "-fx-alignment: center; " +
+                                    "-fx-text-alignment: center;"
+                            );
+                            ImageView gameImageView = new ImageView(new Image(game.getBackground_image()));
+                            gameImageView.setFitHeight(80);
+                            gameImageView.setFitWidth(300);
+                            javafx.scene.control.Label gameLabel = new javafx.scene.control.Label(game.getName());
+                            gameLabel.setFont(Font.font("Arial", 16));
+                            vbox.getChildren().addAll(gameImageView, gameLabel);
+                            setGraphic(vbox);
                         }
                     }
                 };
             }
         });
+        gamesListView.setFixedCellSize(100);
+        gamesListView.setPrefHeight(600);
         gamesListView.getItems().setAll(currentUser.getFavGames());
     }
 }
