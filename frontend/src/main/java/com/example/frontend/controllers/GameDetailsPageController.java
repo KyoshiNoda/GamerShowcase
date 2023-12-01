226caa52b875666da249784725a6bb738c019316
murrmc <murrmc@farmingdale.edu>
2023-12-01 11:21:45 -0500

    Everything works!

--- frontend/src/main/java/com/example/frontend/controllers/GameDetailsPageController.java
+++ frontend/src/main/java/com/example/frontend/controllers/GameDetailsPageController.java
@@ -3,6 +3,10 @@ package com.example.frontend.controllers;
 import com.example.frontend.App;
 import com.example.frontend.Game;
 import com.example.frontend.User;
+import com.example.frontend.controllers.user_setting.ChangeEmailController;
+import com.example.frontend.controllers.user_setting.ChangeFirstNameController;
+import com.example.frontend.controllers.user_setting.ChangeLastNameController;
+import com.example.frontend.controllers.user_setting.ChangePasswordController;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.SetOptions;
@@ -32,17 +36,21 @@ import static com.example.frontend.controllers.MainPageController.clickedGame;
 public class GameDetailsPageController {
 
     @FXML
-    private TextField RatingTextField;
+    private Label RatingLabel;
     @FXML
     private TextArea DescriptionTextArea;
     @FXML
-    private TextField ReleaseTextField;
+    private Label ReleaseLabel;
     @FXML
     private ImageView GameImageView;
     @FXML
-    private TextField ESRBTextField;
+    private Label ESRBLabel;
+    @FXML
+    private Label NameLabel;
     @FXML
     private Button BackButton;
+    @FXML
+    private Button FavoriteGameButton;
     @FXML
     private User currentUser;
     @FXML
@@ -51,10 +59,34 @@ public class GameDetailsPageController {
     private void LoadGameDetails(Game game) throws Exception {
         String imageUrl = game.getBackground_image();
         GameImageView.setImage(new Image(imageUrl));
-        RatingTextField.setText(game.getRating());
+        NameLabel.setText(game.getName());
+        RatingLabel.setText(game.getRating());
         DescriptionTextArea.setText(getGameDescription(game));
-        ReleaseTextField.setText(game.getReleased());
-        ESRBTextField.setText(game.getEsrb());
+        ReleaseLabel.setText(game.getReleased());
+        ESRBLabel.setText(game.getEsrb());
+    }
+
+    @FXML
+    protected void HandleaddFavoriteGame() {
+        addFavoriteGame(clickedGame);
+    }
+    @FXML
+    protected void addFavoriteGame(Game game) {
+        if (!currentUser.gameExists(game)){
+            currentUser.getFavGames().add(game);
+            DocumentReference userRef = App.db.collection("Users").document(currentUser.getId());
+            Map<String, Object> updateData = new HashMap<>();
+            updateData.put("favGames", currentUser.getFavGames());
+            try {
+                ApiFuture<WriteResult> updateFuture = userRef.set(updateData, SetOptions.merge());
+                updateFuture.get();
+                showAlert("Game added to favorites successfully", Alert.AlertType.INFORMATION);
+            } catch (InterruptedException | ExecutionException e) {
+                showAlert("Failed to add game to favorites. Please try again.", Alert.AlertType.ERROR);
+            }
+        } else {
+            showAlert("Already Favorited Game!", Alert.AlertType.ERROR);
+        }
     }
 
     public void setUserData(User currentUser) {
@@ -63,13 +95,34 @@ public class GameDetailsPageController {
 
 
 
+
+
     @FXML
     protected void Return_To_Main_Page() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/frontend/main-page.fxml"));
-        App.setRoot("main-page");
+        loadScene("/com/example/frontend/main-page.fxml");
     }
 
+    private void loadScene(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Scene scene = new Scene(loader.load(), 600, 1000);
+            MainPageController mainPageController = loader.getController();
+            mainPageController.setUserData(currentUser);
+            Stage stage = (Stage) NameLabel.getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    private void showAlert(String message, Alert.AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle("Favorite Game Status");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 
 
 }
